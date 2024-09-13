c0d4e05a2b0d1026a29cdeb94ae4d180cedfd1f7
george <george@ishaq.com>
2024-09-13 16:23:49 -0700

    Fixed web_scraper, now gets parsed_desc and title

--- web_scraper.py
+++ web_scraper.py
@@ -1,57 +1,53 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
+from lxml import html
+from time import sleep
 
-driver = webdriver.Chrome()
+def get_prod_title_desc(url):
+    """
+    Given an aliexpress product URL, this function will scrape the product title and description
 
-driver.get("https://www.aliexpress.com/item/1005005599682662.html?spm=a2g0o.productlist.main.1.569451b4xtESs3&algo_pvid=470ee236-4d63-4040-9b4c-5f6cdcc7646a&utparam-url=scene%3Asearch%7Cquery_from%3A")
+    RETURNS
+    title: str - the product title
+    parsed_desc: str - the product description
+    """
+    driver = webdriver.Chrome()
+    driver.get(url)
+    sleep(2)
 
-driver.implicitly_wait(2)
+    SCROLL_PAUSE_TIME = 4  # Adjust based on how fast the page loads
 
-first_dropdown = driver.find_element(By.CLASS_NAME, "ship-to--menuItem--WdBDsYl")
-first_dropdown.click()
+    # Get the initial scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
 
-language_dropdown = driver.find_element(By.XPATH, "//div[contains(span/text(), 'العربية')]")
-language_dropdown.click()
-dropdown = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//div[contains(span/text(), 'العربية')]/following-sibling::div"))
-)
+    while True:
+        # Scroll down by 3000 pixels
+        driver.execute_script("window.scrollBy(0, 3000);")
 
-english_option = driver.find_element(By.XPATH, "//div[contains(text(), 'English')]")
-english_option.click()
+        # Wait to load more content
+        sleep(SCROLL_PAUSE_TIME)
 
-#product_description_div = driver.find_element(By.ID, "root")
-save_button = driver.find_element(By.CLASS_NAME, "es--saveBtn--w8EuBuy")
-save_button.click()
+        # Calculate new scroll height after scrolling
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        print(last_height, new_height)
+        # Break the loop if no new content has loaded (i.e., scroll height hasn't changed)
+        if new_height == last_height:
+            break
 
-WebDriverWait(driver, 10).until(EC.url_changes(driver.current_url))
-driver.execute_script("window.scrollBy(0, 1000);")
+        # Update the last height to the new height
+        last_height = new_height
 
-product_description_div = driver.find_element(By.XPATH, "//div[starts-with(@class, 'description--origin-part--')]")
-#print('product description div 2:', product_description_div.get_attribute('innerHTML'))
+    tree = html.fromstring(driver.page_source)
 
-text_content = ""
-image_urls = []
-for paragraph in product_description_div.find_elements(By.TAG_NAME, "p"):
-    # Check if the paragraph contains text within a <span>
-    #print('paragraph:', paragraph.get_attribute('innerHTML'), paragraph.find_elements(By.TAG_NAME, "img"))
-    try:
-        #print(paragraph.find_elements(By.TAG_NAME, "img"))
-        if paragraph.find_elements(By.TAG_NAME, "img"):
-            # If it contains images, extract the URLs and append them to the image_urls list
-            print(f'in {paragraph.find_element(By.TAG_NAME, "img").get_attribute("src")}')
-            image_urls.append(paragraph.find_element(By.TAG_NAME, "img").get_attribute("src"))
-            #image_urls.extend([img.get_attribute("src") for img in paragraph.find_elements_by_tag_name("img")])
-        elif paragraph.find_element(By.TAG_NAME, "span"):
-            # If it contains text, append it to the text_content string
-            text_content += paragraph.text + " "
-        
-    except:
-        pass
-print('done')
-print('content:', text_content,'\n \n', image_urls)
+    #get the title and description
+    title = tree.xpath("//div[contains(@class, 'title--wrap')]")[0].text_content()
+    desc = tree.xpath("//div[contains(@id, 'product-description')]")
+    parsed_desc = ""
 
-while True:
-    pass
+    #parse the description into one string
+    for child in desc[0]:
+        parsed_desc += child.text_content().strip() + "\n"
+
+    driver.quit()
+    return title, parsed_desc
+
+print(get_prod_title_desc('https://vi.aliexpress.com/item/1005005707583364.html'))
